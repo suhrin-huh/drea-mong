52596999706f495ae4b8b3ce74d680d8273bb824
서규범 <seo980916@gmail.com>
2024-07-30 13:44:41 +0900

    fix : Button.jsx 피드백 반영, tailwind.config.js 수정

--- frontend/dreamong/src/components/Button.jsx
+++ frontend/dreamong/src/components/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ children, click, rounded, isFilled, isFull, isShadow }) => {
+const Button = ({ children, click, rounded = 'lg', isFilled = true, isFull = false, isShadow = false }) => {
   let buttonClasses = isFull === true ? 'w-full m-2 select-none px-2 py-3' : 'm-2 select-none px-6 py-3';
 
   // 자료형이 boolean일 때만 적용되도록 조건 직접 명시
   if (isFilled === true) {
-    buttonClasses += ` rounded-${rounded} text-white bg-indigo-700 hover:bg-indigo-900 hover:ring-2 hover:ring-offset-2 hover:ring-offset-slate-50 hover:ring-indigo-300`;
+    buttonClasses += ` rounded-${rounded} text-white bg-primary-500 hover:bg-primary-700`;
   } else if (isFilled === false) {
-    buttonClasses += ` rounded-${rounded} text-black bg-white hover:bg-gray-100 hover:ring-2 hover:ring-offset-2 hover:ring-offset-slate-50 hover:ring-gray-300`;
+    buttonClasses += ` rounded-${rounded} text-black bg-white hover:bg-gray-100`;
   }
 
   if (isShadow === true) {
@@ -18,7 +18,5 @@ const Button = ({ children, click, rounded, isFilled, isFull, isShadow }) => {
     </button>
   );
 };
-
-Button.defaultProps = { rounded: 'lg', isFilled: true, isFull: false, isShadow: false };
 
 export default Button;
