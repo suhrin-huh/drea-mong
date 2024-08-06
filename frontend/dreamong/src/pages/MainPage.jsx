29a56369cbfb0a99e8b9901622503bbb8fc4ac5d
황민채 <trick0846@naver.com>
2024-08-06 17:56:44 +0900

    chore: axios 설정 및 baseURL 설정

--- frontend/dreamong/src/pages/MainPage.jsx
+++ frontend/dreamong/src/pages/MainPage.jsx
@@ -1,6 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StatisticsIcon } from '../assets/icons';
+import api from '../utils/api';
+import { userState } from '../recoil/atoms';
+import { useRecoilValue } from 'recoil';
 
 // Import Swiper React components
 // npm install swiper
@@ -20,6 +23,7 @@ const MainPage = () => {
   const [dreams, setDreams] = useState({});
   const [year, setYear] = useState(current.getFullYear());
   const [month, setMonth] = useState(current.getMonth() + 1);
+  const user = useRecoilValue(userState);
 
   // 월에 포커스를 맞추기 위해서
   const swiperRef = useRef(null);
@@ -27,195 +31,20 @@ const MainPage = () => {
   // 초기 렌더링시에 처리되는 일
 
   useEffect(() => {
-    const current = new Date();
-    // 1. 꿈리스트 받아오기
-    setDreams({
-      username: '민채',
-      totalCount: 5,
-      dreams: [
-        {
-          dreamId: 1,
-          content: '고구마 무스에 소금을 뿌려서...',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240701',
-        },
-        {
-          dreamId: 2,
-          content: '하루종일 산책만 해서...',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240702',
-        },
-        {
-          dreamId: 3,
-          content: '더미 꿈 내용 3',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240703',
-        },
-        {
-          dreamId: 4,
-          content: '더미 꿈 내용 4',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240704',
-        },
-        {
-          dreamId: 5,
-          content: '더미 꿈 내용 5',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240705',
-        },
-        {
-          dreamId: 6,
-          content: '더미 꿈 내용 6',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240706',
-        },
-        {
-          dreamId: 7,
-          content: '더미 꿈 내용 7',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240707',
-        },
-        {
-          dreamId: 8,
-          content: '더미 꿈 내용 8',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240708',
-        },
-        {
-          dreamId: 9,
-          content: '더미 꿈 내용 9',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240709',
-        },
-        {
-          dreamId: 10,
-          content: '더미 꿈 내용 10',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240710',
-        },
-        {
-          dreamId: 11,
-          content: '더미 꿈 내용 11',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240711',
-        },
-        {
-          dreamId: 12,
-          content: '더미 꿈 내용 12',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240712',
-        },
-        {
-          dreamId: 13,
-          content: '더미 꿈 내용 13',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240713',
-        },
-        {
-          dreamId: 14,
-          content: '더미 꿈 내용 14',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240714',
-        },
-        {
-          dreamId: 15,
-          content: '더미 꿈 내용 15',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240715',
-        },
-        {
-          dreamId: 16,
-          content: '더미 꿈 내용 16',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240716',
-        },
-        {
-          dreamId: 17,
-          content: '더미 꿈 내용 17',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240717',
-        },
-        {
-          dreamId: 18,
-          content: '더미 꿈 내용 18',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240718',
-        },
-        {
-          dreamId: 19,
-          content: '더미 꿈 내용 19',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240719',
-        },
-        {
-          dreamId: 20,
-          content: '더미 꿈 내용 20',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240720',
-        },
-        {
-          dreamId: 21,
-          content: '더미 꿈 내용 21',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240721',
-        },
-        {
-          dreamId: 22,
-          content: '더미 꿈 내용 22',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240722',
-        },
-        {
-          dreamId: 23,
-          content: '더미 꿈 내용 23',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240723',
-        },
-        {
-          dreamId: 24,
-          content: '더미 꿈 내용 24',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240724',
-        },
-        {
-          dreamId: 25,
-          content: '더미 꿈 내용 25',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240725',
-        },
-        {
-          dreamId: 26,
-          content: '더미 꿈 내용 26',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240726',
-        },
-        {
-          dreamId: 27,
-          content: '더미 꿈 내용 27',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240727',
-        },
-        {
-          dreamId: 28,
-          content: '더미 꿈 내용 28',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240728',
-        },
-        {
-          dreamId: 29,
-          content: '더미 꿈 내용 29',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240729',
-        },
-        {
-          dreamId: 30,
-          content: '더미 꿈 내용 30',
-          image: '/src/assets/MainpageTest.jpg',
-          writeDate: '20240730',
-        },
-      ],
-    });
+    const fetchData = async () => {
+      try {
+        console.log(`/${user.userId}/${year}${String(month).padStart(2, '0')}`);
+        const response = await api.get(`/dream/${user.userId}/${year}${String(month).padStart(2, '0')}01`); // 요청을 보낼 엔드포인트
+        setDreams(response.data);
+        console.log(response);
+        console.log(dreams);
+      } catch (err) {
+        console.log(`/dream/${user.userId}/${year}${String(month).padStart(2, '0')}01`);
+        setError(err);
+      }
+    };
 
+    fetchData();
     // 2. 날짜에 맞게 포커스
     // month는 7월, silde index는 (7-1)월이므로 알맞게 수정
     swiperRef.current?.swiper.slideTo(month - 1, 0);
@@ -256,9 +85,9 @@ const MainPage = () => {
         <p className="text-sm">{dreams.totalCount}번째 꿈을 기록해주세요!!</p>
       </header>
       {/* 아랫부분부터 메인 내용 들어가는 페이지 */}
-      <div className="flex-col items-center w-full gap-2 px-4 py-3 mx-auto text-center bg-white h-3/4 rounded-t-3xl">
+      <div className="mx-auto h-3/4 w-full flex-col items-center gap-2 rounded-t-3xl bg-white px-4 py-3 text-center">
         {/* 날짜 선택 */}
-        <div className="w-full py-4 text-center h-1/5">
+        <div className="h-1/5 w-full py-4 text-center">
           {/* 연도 선택 */}
           <div className="flex items-center justify-center gap-12 p-2.5 text-xl">
             <button onClick={() => handleYear(-1)}>{'<'}</button>
@@ -266,7 +95,7 @@ const MainPage = () => {
             <button onClick={() => handleYear(1)}>{'>'}</button>
           </div>
           {/* 월 선택 */}
-          <div className="flex items-center justify-center gap-1 my-2 text-base align-center">
+          <div className="align-center my-2 flex items-center justify-center gap-1 text-base">
             <Swiper
               ref={swiperRef}
               slidesPerView={6}
@@ -276,7 +105,7 @@ const MainPage = () => {
               modules={[Pagination]}
               slideToClickedSlide={true}
               //Swiper 컴포넌트에서 슬라이드를 클릭하면 해당 슬라이드가 중심에 오도록 설정하려면, Swiper 컴포넌트의 slideToClickedSlide 속성을 사용해야 합니다. 이 속성은 클릭한 슬라이드로 자동으로 이동하게 합니다.
-              className="h-full mySwiper"
+              className="mySwiper h-full"
             >
               {/* 1월부터 12월까지 */}
               {Array.from({ length: 12 }).map((_, i) => (
@@ -299,15 +128,14 @@ const MainPage = () => {
           </div>
         </div>
 
-        
         {/* 이부분에 일기 들어가기 */}
-        <div className="flex-col my-2 overflow-y-auto h-3/4">
-          {dreams.dreams &&
+        <div className="my-2 h-3/4 flex-col overflow-y-auto">
+          {dreams.dreams ?
             dreams.dreams.map((dream) => {
               return (
-                <div className="flex items-start justify-center h-20 my-2 gap-x-3">
+                <div className="my-2 flex h-20 items-start justify-center gap-x-3">
                   <div className="mt-2 h-[60px] w-1/6 flex-col items-center justify-start">
-                    <div className="text-3xl font-bold text-center">{dream.writeDate.slice(6, 8)}</div>
+                    <div className="text-center text-3xl font-bold">{dream.writeDate.slice(6, 8)}</div>
                     <div className="text-slate-500">{getWeekDay(dream.writeDate)}</div>
                   </div>
                   <div
@@ -323,7 +151,7 @@ const MainPage = () => {
                   </div>
                 </div>
               );
-            })}
+            }): <div className={}></div>}
         </div>
       </div>
     </div>
