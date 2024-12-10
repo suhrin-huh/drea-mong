1de5d6f383a33f9a233659a1170b7c8b91adc0d0
황민채 <trick0846@naver.com>
2024-12-11 01:39:22 +0900

    refactor: code splitting 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { useState } from 'react';
+import { useState, Suspense, lazy } from 'react';
 
 import NavigationBar from './components/NavigationBar';
-import MainPage from './pages/MainPage/MainPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import LoginSuccess from './pages/LoginPage/components/LoginSuccess';
-import DreamRegisterPage from './pages/DreamPage/DreamRegisterPage';
-import DreamDetailPage from './pages/DreamPage/DreamDetailPage';
-import SettingsPage from './pages/SettingsPage';
-import StreamingPage from './pages/StreamingPage/StreamingPage';
-import StreamingList from './pages/StreamingPage/components/StreamingList';
-import StreamingRoom from './pages/StreamingPage/components/StreamingRoom';
-import SquarePage from './pages/SquarePage';
-import SquareDetailPage from './pages/SquareDetailPage';
-import StatisticsSkeletonPage from './pages/SkeletonPage/StatisticsSkeletonPage';
-import StatisticsPage from './pages/StatisticsPage';
 import ErrorPage from './pages/ErrorPage';
+
+const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const LoginSuccess = lazy(() => import('./pages/LoginPage/components/LoginSuccess'));
+const DreamRegisterPage = lazy(() => import('./pages/DreamPage/DreamRegisterPage'));
+const DreamDetailPage = lazy(() => import('./pages/DreamPage/DreamDetailPage'));
+const SettingsPage = lazy(() => import('./pages/SettingsPage'));
+const StreamingPage = lazy(() => import('./pages/StreamingPage/StreamingPage'));
+const StreamingList = lazy(() => import('./pages/StreamingPage/components/StreamingList'));
+const StreamingRoom = lazy(() => import('./pages/StreamingPage/components/StreamingRoom'));
+const SquarePage = lazy(() => import('./pages/SquarePage'));
+const SquareDetailPage = lazy(() => import('./pages/SquareDetailPage'));
+const StatisticsPage = lazy(() => import('./pages/StatisticsPage'));
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState();
@@ -32,33 +32,31 @@ function App() {
           <div className="relative flex h-screen w-full max-w-[600px] flex-col bg-[url('/src/assets/background.svg')] bg-cover bg-center">
             <main className="flex-1 overflow-auto">
               <div className="min-h-full pb-[60px]">
-                {' '}
-                {/* NavigationBar 높이만큼 패딩 추가 */}
-                <Routes>
-                  <Route exact path="/" element={<MainPage />} />
-                  <Route path="/dream/create" element={<DreamRegisterPage />} />
-                  <Route path="/dream/:dreamId" element={<DreamDetailPage />} />
-                  {/* <Route path="/dream/:dreamId/update" element={} /> */}
-                  <Route path="/square/dreams" element={<SquarePage />} />
-                  <Route path="/square/:dreamId" element={<SquareDetailPage />} />
-                  <Route path="/streaming" element={<StreamingPage />}>
-                    <Route index element={<StreamingList />} />
-                    {/* <Route path="create" element={} /> */}
-                    <Route path=":roomId" element={<StreamingRoom />} />
-                  </Route>
-                  <Route path="/statistics" element={<StatisticsPage />} />
-                  <Route path="/settings" element={<SettingsPage />} />
-                  <Route
-                    path="/login"
-                    element={<LoginPage isLoggedIn={isLoggedIn} toggleLoginStatus={toggleLoginStatus} />}
-                  />
-                  <Route
-                    path="/callback"
-                    element={<LoginSuccess isLoggedIn={isLoggedIn} toggleLoginStatus={toggleLoginStatus} />}
-                  />
-                  <Route path="*" element={<ErrorPage />} /> {/* 존재하지 않는 경로 처리 */}
-                  <Route path="/error" element={<ErrorPage />} /> {/* /error 경로 처리 */}
-                </Routes>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Routes>
+                    <Route exact path="/" element={<MainPage />} />
+                    <Route path="/dream/create" element={<DreamRegisterPage />} />
+                    <Route path="/dream/:dreamId" element={<DreamDetailPage />} />
+                    <Route path="/square/dreams" element={<SquarePage />} />
+                    <Route path="/square/:dreamId" element={<SquareDetailPage />} />
+                    <Route path="/streaming" element={<StreamingPage />}>
+                      <Route index element={<StreamingList />} />
+                      <Route path=":roomId" element={<StreamingRoom />} />
+                    </Route>
+                    <Route path="/statistics" element={<StatisticsPage />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                    <Route
+                      path="/login"
+                      element={<LoginPage isLoggedIn={isLoggedIn} toggleLoginStatus={toggleLoginStatus} />}
+                    />
+                    <Route
+                      path="/callback"
+                      element={<LoginSuccess isLoggedIn={isLoggedIn} toggleLoginStatus={toggleLoginStatus} />}
+                    />
+                    <Route path="*" element={<ErrorPage />} />
+                    <Route path="/error" element={<ErrorPage />} />
+                  </Routes>
+                </Suspense>
               </div>
             </main>
             <NavigationBar />
